d22ae98be54236d1a9bc93280a28bff5af51669c
TeoSparkles <catapusantp@byui.edu>
2020-10-07 15:45:19 +0800

    Initial commit

---
+++ src/Teo/HelloFromTeo.java
@@ -0,0 +1,10 @@
+package Teo;
+
+public class HelloFromTeo {
+
+    public static void main(String[] args) {
+	// write your code here
+        String name = "Teo";
+        System.out.println("Hello, my name is " + name);
+    }
+}
